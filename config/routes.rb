16abc28ee440e27e16768d3fc9264f9f4f8e3543
J-Henry <JH@JH.local>
2016-04-21 13:31:00 -0400

    Setup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-
+  # resources :notes, path: 'api', as: :notes
+  get  '/api/notes' => 'notes#index',  as: :root
+  post '/api/notes' => 'notes#create', as: :new_note
+  get '/api/notes/tag/:name' => 'notes#show', as: :by_tag
 end
