40105b46f3b6143582812fac35b65e0a0c6d4465
Ryder Timberlake <wrtimberlake@gmail.com>
2016-04-21 14:07:41 -0400

    Finished index view for notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -4,4 +4,18 @@ class NotesController < ApplicationController
     @notes = Note.all
   end
 
+  def create
+    @note = Note.new(note_params)
+    if @note.save
+    else
+      render :errors, status: 400
+    end
+  end
+
+  private
+
+  def note_params
+    params.permit(:title, :body)
+  end
+
 end
--- app/views/notes/index.json.jbuilder
+++ app/views/notes/index.json.jbuilder
@@ -1,4 +1,9 @@
 json.notes
   json.array! @notes do |note|
     json.(note, :title, :body, :created_at, :updated_at)
+    json.tags do
+      json.array! note.tags do |tag|
+        json.name
+      end
+    end
   end
