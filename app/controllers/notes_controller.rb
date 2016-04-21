3be4879c9400d0a6056aeffd8e7b53ea14fe059c
Seth Baughman <sbaughman1@me.com>
2016-04-21 15:29:07 -0400

    modify create action to include tags

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -7,7 +7,10 @@ class NotesController < ApplicationController
   def create
     @note = Note.new(note_params)
     if @note.save
+      add_tags(@note)
+      render :show, status: :created
     else
+      render :errors, status: 400
     end
   end
 
@@ -15,6 +18,14 @@ class NotesController < ApplicationController
 
   def note_params
     params.permit(:title, :body)
+  end
+
+  def add_tags(note)
+    tags_array = params[:tags].split(",").collect(&:strip)
+    tags_array.each do |tag|
+      new_tag = Tag.find_or_create_by(name: tag)
+      Tagging.create(tag_id: new_tag.id, note_id: note.id)
+    end
   end
 
 end
