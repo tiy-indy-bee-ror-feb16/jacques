7e9efc66f2afe4b683cde1da242389d0e3e8bf67
Kurt Friedrich <kurt.is.friedrich@gmail.com>
2016-04-21 17:38:51 -0400

    create models

---
+++ app/views/tags/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @tag, :id, :name, :note_id, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160421213726) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "notes", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "note_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "tags", ["note_id"], name: "index_tags_on_note_id", using: :btree
+
+  add_foreign_key "tags", "notes"
 end
