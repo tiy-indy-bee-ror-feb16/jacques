80cd655fe4f9174220bdbcbf40c09b37eed0dfb6
J-Henry <JH@JH.local>
2016-04-21 16:07:03 -0400

    Complete models note, tag, tagging

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160421174111) do
+ActiveRecord::Schema.define(version: 20160421194254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,4 +23,26 @@ ActiveRecord::Schema.define(version: 20160421174111) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "taggings", force: :cascade do |t|
+    t.integer  "tag_id"
+    t.integer  "note_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "taggings", ["note_id"], name: "index_taggings_on_note_id", using: :btree
+  add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id", using: :btree
+
+  create_table "tags", force: :cascade do |t|
+    t.integer  "note_id"
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "tags", ["note_id"], name: "index_tags_on_note_id", using: :btree
+
+  add_foreign_key "taggings", "notes"
+  add_foreign_key "taggings", "tags"
+  add_foreign_key "tags", "notes"
 end
