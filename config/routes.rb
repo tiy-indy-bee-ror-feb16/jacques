94a30efe9a7019f0b64c04733345f3ec059709fa
Ryder Timberlake <wrtimberlake@gmail.com>
2016-04-21 16:13:28 -0400

    Built route for tags#show, add json format to tag_path, add show to tags controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
 
   scope '/api' do
     resources :notes, defaults: { format: 'json' }
+    get 'notes/tag/:tag_name' => 'tags#show', as: :tag, defaults: { format: 'json' }
   end
+
 end
