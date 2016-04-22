b94a69510d36e80886a6ff9ab89c2844c2f9fd3b
Ryder Timberlake <wrtimberlake@gmail.com>
2016-04-22 13:22:51 -0400

    Add users resoure

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :users
   scope '/api' do
     resources :notes, defaults: { format: 'json' }
     get 'notes/tag/:tag_name' => 'tags#show', as: :tag, defaults: { format: 'json' }
