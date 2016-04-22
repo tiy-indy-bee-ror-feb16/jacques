2a7c25c479c60380af7f83568fa51927b9748b0a
Seth Baughman <sbaughman1@me.com>
2016-04-22 15:08:03 -0400

    index request returns user's notes if api_token given. notes created with api_token have user_id assigned

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,8 @@ class ApplicationController < ActionController::API
     @user = User.find_by(email: params[:email])
   end
 
+  def authenticate_user
+    @user = User.find_by(api_token: params[:api_token])
+  end
+
 end
